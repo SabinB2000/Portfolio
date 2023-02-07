77c9ab4538b30d809750b4514632a4e52c7472d9
Sabin <np03cs4a220055@heraldcollege.edu.np>
2023-02-07 19:10:49 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyLvAweoOQ9h9rJtj5hczCotAoEeD01StTBgVN5j97TiPbJTJx3UcVPzAlwCc_xNkMx/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg=document.getElementById("msg")
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//       msg.innerHTML = "Message Sent Sucessfully"
+//       setTimeout(function(){
+//       msg.innerHTML=""
+//   },5000)
+//   form.reset()
+// })
+//     .catch(error => console.error('Error!', error.message))
+// })
